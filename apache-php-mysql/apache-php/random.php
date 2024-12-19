36e15c2fc9674183f06c6ee912903b131fec2b1e
alvaro <alvarodevejer4@gmail.com>
2024-12-19 10:27:32 +0100

    sprint 4:php

--- apache-php-mysql/apache-php/random.php
+++ apache-php-mysql/apache-php/random.php
@@ -1,19 +1,20 @@
 <?php
-$randomNumber = rand(1,100); // Número aleatorio
+$randomNumber = rand(1, 100); // Número aleatorio
 
 $isEven = ($randomNumber % 2 === 0) ? "par" : "impar";
 
-$elements = ["silla", "teclado", "monitor", "altavoces", "ratón"]
+$elements = ["silla", "teclado", "monitor", "altavoces", "ratón"]; // Correctamente terminado con punto y coma
 
 // Crear respuesta para enviar como JSON
 $response = [
-    "numero_aleaorio" = $randomNumber,
-    "par_impar" = $isEven,
-    "elemento_aleatorio" = $elements
+    "numero_aleatorio" => $randomNumber,   
+    "par_impar" => $isEven,                 
+    "elemento_aleatorio" => $elements      
 ];
 
 // Encabezado para respuesta como JSON
-header('Content-Type: application/json')
+header('Content-Type: application/json');
 
-echo json_encode($response)
+// Enviar la respuesta como JSON
+echo json_encode($response);
 ?>
